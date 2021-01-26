2b56f7572415fafe13819d12595236e2b7dd4659
vũ <vanvux99@gmail.com>
2021-01-26 17:41:19 +0700

    đẩy file cuối ngày

---
+++ Controllers/BlogsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CShap_MVC_VuDoan.Models;
+
+namespace CShap_MVC_VuDoan.Controllers
+{
+    public class BlogsController : Controller
+    {
+        // GET: Blogs
+        [HttpGet]
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult List(Blogs blog)
+        {
+            return View(blog);
+        }
+
+        [HttpGet]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(Blogs blog)
+        {
+            return View(blog);
+        }
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(Blogs blog)
+        {
+            return View(blog);
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Blogs blog)
+        {
+            return View(blog);
+        }
+    }
+}
---
+++ Models/Blogs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CShap_MVC_VuDoan.Models
+{
+    public class Blogs
+    {
+        [Key]
+        public int ID { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Your Title")]
+        public string TITLE { get; set; }
+
+
+        public string DESCRIPTION_SHORT { get; set; }
+
+
+        public string DETAIL { get; set; }
+
+
+        public string IMAGE { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Your Title")]
+        public int CATEGORY { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Your Title")]
+        public string LOCATIONS { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Your Title")]
+        public int PUBLICS { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Your Title")]
+        public DateTime DATE_PUBLIC { get; set; }
+
+
+    }
+}
